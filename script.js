e9f1912c84ebe14a016b6a85b0993fb37412c449
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-12-24 18:10:57 -0800

    pseudo

---
+++ script.js
@@ -0,0 +1,18 @@
+//goal is to have as little global code as possible. Going hard on encapsulation
+//If only need one instance of something, warp it in an IIFE/module pattern so that it cannot be reused
+//Odin advises completing game in console before worrying about DOM at all
+function gameboard(){
+    gameboard = []; //holds array of squares/spots
+    //3x3
+}
+function player(){
+    //x player or o player
+    //my turn or not my turn
+    //place x/o command
+
+}
+function gameController(){
+    //win condition checker
+    //whose turn is it
+    //thats it(?)
+}
